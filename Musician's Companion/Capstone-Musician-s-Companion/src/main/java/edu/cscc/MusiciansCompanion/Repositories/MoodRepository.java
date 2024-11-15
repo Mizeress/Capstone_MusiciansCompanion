51bf4a9baeee9996711a0570c7eb3eabf6ffd190
Mizeress <sam.mallia101@gmail.com>
2024-11-15 12:57:10 -0500

    Delete Musician's Companion/Capstone-Musician-s-Companion directory
    This was renamed to a more reasonable name

--- Musician's Companion/Capstone-Musician-s-Companion/src/main/java/edu/cscc/MusiciansCompanion/Repositories/MoodRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.cscc.MusiciansCompanion.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import edu.cscc.MusiciansCompanion.Models.Mood;
-
-public interface MoodRepository extends CrudRepository<Mood, Long> {
-
-}
--- Musician's Companion/Capstone-Musician-s-Companion/src/main/java/edu/cscc/MusiciansCompanion/Repositories/TimeSignatureRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.cscc.MusiciansCompanion.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import edu.cscc.MusiciansCompanion.Models.TimeSignature;
-
-public interface TimeSignatureRepository extends CrudRepository<TimeSignature, Long> {
-
-}
